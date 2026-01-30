86458a3adb1520d6b3fef829219c36823cd931dc
Егор Подделков <gulmix@bk.ru>
2026-01-30 23:26:36 +0300

    done with registration and login UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import Home from './components/home/Home'
+import Header from './components/header/Header'
+import Register from './components/register/Register'
+import Login from './components/login/Login'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Home/>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
     </>
   )
 }
